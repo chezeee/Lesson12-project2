76b03b352cd79ae41dbd00577a5eb0c6a202e8c6
Ilya Yalymov <chezeee@gmail.com>
2023-01-23 21:55:00 +0300

    Initial structure added

---
+++ js/classroom.js
@@ -0,0 +1,29 @@
+/** @param {number[]} grades */
+export function getNumberOfGrades(grades) {
+  // your code
+}
+
+/** @param {number[]} grades */
+export function getSumGrades(grades) {
+  // your code
+}
+
+/** @param {number[]} grades */
+export function getAverageValue(grades) {
+  // your code
+}
+
+/** @param {number[]} grades */
+export function getPassingGrades(grades) {
+  // your code
+}
+
+/** @param {number[]} grades */
+export function getFailingGrades(grades) {
+  // your code
+}
+
+/** @param {number[]} grades */
+export function getRaisedGrades(grades) {
+  // your code
+}
